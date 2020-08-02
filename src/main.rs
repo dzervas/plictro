a62f8d60b9acc72d04d3b2559050b83b9ce1d258
Dimitris Zervas <dzervas@noc.grnet.gr>
2020-08-02 21:24:26 +0300

    Back to the basics

--- src/main.rs
+++ src/main.rs
@@ -1,151 +1,49 @@
 #![no_std]
 #![no_main]
-#![warn(rust_2018_idioms)]
 
-mod hid_service;
+extern crate cortex_m_rt;
+extern crate nb;
+extern crate nrf52840_hal;
+extern crate panic_semihosting;
 
-// We need to import this crate explicitly so we have a panic handler
-use panic_semihosting as _;
+use cortex_m_rt::entry;
+use nb::block;
+use nrf52840_dk_bsp::Board;
+// use nrf52840_dk_bsp::hal as hal;
+use nrf52840_dk_bsp::hal::prelude::*;
+use nrf52840_dk_bsp::hal::gpio::Level;
+use nrf52840_dk_bsp::hal::timer::{self, Timer};
 
-use nrf52840_hal as hal;
+#[entry]
+fn main() -> ! {
 
-use rubble::l2cap::{BleChannelMap, L2CAPState};
-use rubble::link::queue::{PacketQueue, SimpleQueue};
-use rubble::link::{ad_structure::AdStructure, LinkLayer, Responder, MIN_PDU_BUF};
-use rubble::time::{Duration, Timer};
-use rubble::{config::Config, security::NoSecurity};
-use rubble_nrf5x::radio::{BleRadio, PacketBuffer};
-use rubble_nrf5x::{timer::BleTimer, utils::get_device_address};
+	let board = Board::take().unwrap();
 
-pub enum AppConfig {}
+	let mut timer = Timer::new(board.TIMER4);
+	let mut led = board.pins.P0_28.into_push_pull_output(Level::High);
+	led.set_high().unwrap();
 
-impl Config for AppConfig {
-	type Timer = BleTimer<hal::pac::TIMER0>;
-	type Transmitter = BleRadio;
-	type ChannelMapper = BleChannelMap<hid_service::HIDServiceAttrs, NoSecurity>;
-	type PacketQueue = &'static mut SimpleQueue;
+	led.set_low().unwrap();
+	let mut is_high = false;
+
+	loop {
+		if is_high {
+			led.set_low().unwrap();
+			is_high = false;
+		} else {
+			led.set_high().unwrap();
+			is_high = true;
+		}
+
+		delay(&mut timer, 333_333);
+	}
+
 }
 
-#[rtic::app(device = crate::hal::pac, peripherals = true)]
-const APP: () = {
-	struct Resources {
-		#[init([0; MIN_PDU_BUF])]
-		ble_tx_buf: PacketBuffer,
-		#[init([0; MIN_PDU_BUF])]
-		ble_rx_buf: PacketBuffer,
-		#[init(SimpleQueue::new())]
-		tx_queue: SimpleQueue,
-		#[init(SimpleQueue::new())]
-		rx_queue: SimpleQueue,
-		ble_ll: LinkLayer<AppConfig>,
-		ble_r: Responder<AppConfig>,
-		radio: BleRadio,
-	}
-
-	#[init(resources = [ble_tx_buf, ble_rx_buf, tx_queue, rx_queue])]
-	fn init(ctx: init::Context) -> init::LateResources {
-		// On reset, the internal high frequency clock is already used, but we
-		// also need to switch to the external HF oscillator. This is needed
-		// for Bluetooth to work.
-		let _clocks = hal::clocks::Clocks::new(ctx.device.CLOCK).enable_ext_hfosc();
-
-		let ble_timer = BleTimer::init(ctx.device.TIMER0);
-
-		// Determine device address
-		let device_address = get_device_address();
-
-		let mut radio = BleRadio::new(
-			ctx.device.RADIO,
-			&ctx.device.FICR,
-			ctx.resources.ble_tx_buf,
-			ctx.resources.ble_rx_buf,
-		);
-
-		// Create TX/RX queues
-		let (tx, tx_cons) = ctx.resources.tx_queue.split();
-		let (rx_prod, rx) = ctx.resources.rx_queue.split();
-
-		// Create the actual BLE stack objects
-		let mut ble_ll = LinkLayer::<AppConfig>::new(device_address, ble_timer);
-
-		let ble_r = Responder::new(
-			tx,
-			rx,
-			L2CAPState::new(BleChannelMap::with_attributes(hid_service::HIDServiceAttrs::new())),
-		);
-
-		// Send advertisement and set up regular interrupt
-		let next_update = ble_ll
-			.start_advertise(
-				Duration::from_millis(200),
-				&[AdStructure::CompleteLocalName("Plictro")],
-				&mut radio,
-				tx_cons,
-				rx_prod,
-			)
-			.unwrap();
-
-		ble_ll.timer().configure_interrupt(next_update);
-
-		init::LateResources {
-			radio,
-			ble_ll,
-			ble_r,
-		}
-	}
-
-	#[task(binds = RADIO, resources = [radio, ble_ll], spawn = [ble_worker], priority = 3)]
-	fn radio(ctx: radio::Context) {
-		let ble_ll: &mut LinkLayer<AppConfig> = ctx.resources.ble_ll;
-		if let Some(cmd) = ctx
-			.resources
-			.radio
-			.recv_interrupt(ble_ll.timer().now(), ble_ll)
-		{
-			ctx.resources.radio.configure_receiver(cmd.radio);
-			ble_ll.timer().configure_interrupt(cmd.next_update);
-
-			if cmd.queued_work {
-				// If there's any lower-priority work to be done, ensure that happens.
-				// If we fail to spawn the task, it's already scheduled.
-				ctx.spawn.ble_worker().ok();
-			}
-		}
-	}
-
-	#[task(binds = TIMER0, resources = [radio, ble_ll], spawn = [ble_worker], priority = 3)]
-	fn timer0(ctx: timer0::Context) {
-		let timer = ctx.resources.ble_ll.timer();
-		if !timer.is_interrupt_pending() {
-			return;
-		}
-		timer.clear_interrupt();
-
-		let cmd = ctx.resources.ble_ll.update_timer(ctx.resources.radio);
-		ctx.resources.radio.configure_receiver(cmd.radio);
-
-		ctx.resources
-			.ble_ll
-			.timer()
-			.configure_interrupt(cmd.next_update);
-
-		if cmd.queued_work {
-			// If there's any lower-priority work to be done, ensure that happens.
-			// If we fail to spawn the task, it's already scheduled.
-			ctx.spawn.ble_worker().ok();
-		}
-	}
-
-
-	#[task(resources = [ble_r], priority = 2)]
-	fn ble_worker(ctx: ble_worker::Context) {
-		// Fully drain the packet queue
-		while ctx.resources.ble_r.has_work() {
-			ctx.resources.ble_r.process_one().unwrap();
-		}
-	}
-
-	extern "C" {
-		fn WDT();
-	}
-};
+fn delay<T>(timer: &mut Timer<T>, cycles: u32)
+where
+	T: timer::Instance,
+{
+	timer.start(cycles);
+	let _ = block!(timer.wait());
+}
