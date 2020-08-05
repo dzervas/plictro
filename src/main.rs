507199c1316585638984171ea7f0f7afa8bffe30
Dimitris Zervas <dzervas@noc.grnet.gr>
2020-08-05 22:37:27 +0300

    Add UART panic handler and testing serial in a separate mod

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,41 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_semihosting;
+// extern crate panic_semihosting;
 
-use embedded_hal::timer::Cancel;
+use core::cell::RefCell;
 use core::fmt::Write;
+use core::ops::DerefMut;
+use core::panic::PanicInfo;
 
+use cortex_m::interrupt::{free, Mutex};
 use cortex_m_rt::entry;
-use hal::gpio::{p0, Level};
+
+use hal::gpio::{p0, p1, Level};
 use hal::pac::Peripherals;
 use hal::prelude::*;
-use hal::timer::{Instance, Timer};
-use hal::twim;
-use nb::block;
-use nrf52840_hal as hal;
+pub use nrf52840_hal as hal;
 
-use embedded_graphics::fonts::{Font6x8, Text};
-use embedded_graphics::pixelcolor::BinaryColor;
-use embedded_graphics::prelude::*;
-use embedded_graphics::style::TextStyle;
-use ssd1306::prelude::*;
-use ssd1306::{Builder, I2CDIBuilder};
+// TODO: Don't do the serial init here, use the mod
+// pub mod serial;
+// pub use serial::SERIAL;
+
+pub type SafeSerial = Mutex<RefCell<Option<hal::uarte::Uarte<hal::pac::UARTE0>>>>;
+pub static SERIAL: SafeSerial = Mutex::new(RefCell::new(None));
 
 #[entry]
 fn main() -> ! {
 	let p = Peripherals::take().unwrap();
+	let port0 = p0::Parts::new(p.P0);
+	let _port1 = p1::Parts::new(p.P1);
 
 	// Indicator LED
-	let mut timer = Timer::new(p.TIMER4);
-	let port0 = p0::Parts::new(p.P0);
-
 	let mut led = port0.p0_28.into_push_pull_output(Level::High);
-
-	led.set_low().unwrap();
-	let mut is_high = false;
+	// let mut is_high = true;
+	led.set_high().unwrap();
 
 	// Serial
-	let mut serial = hal::Uarte::new(
+	let serial = hal::Uarte::new(
 		p.UARTE0,
 		hal::uarte::Pins {
 			txd: port0.p0_06.into_push_pull_output(Level::High).degrade(),
@@ -48,69 +47,38 @@ fn main() -> ! {
 		hal::uarte::Baudrate::BAUD115200,
 	);
 
-	writeln!(serial, "Starting Up!").unwrap();
+	free(|cs| {
+		SERIAL.borrow(cs).replace(Some(serial));
+	});
 
-	// Screen
-	let scl = port0.p0_27.into_floating_input().degrade();
-	let sda = port0.p0_26.into_floating_input().degrade();
+	// serial::init();
 
-	let pins = twim::Pins { scl, sda };
+	free(|cs| {
+		if let Some(ref mut s) = SERIAL.borrow(cs).borrow_mut().deref_mut() {
+			writeln!(s, "Initialization complete!").unwrap();
+		}
+	});
 
-	let i2c = twim::Twim::new(p.TWIM0, pins, twim::Frequency::K400);
-
-	let interface = I2CDIBuilder::new().init(i2c);
-	let mut disp: GraphicsMode<_> = Builder::new().connect(interface).into();
-
-	disp.init().expect("Display initialization");
-	disp.flush().expect("Cleans the display");
-
-	let text_style = TextStyle::new(Font6x8, BinaryColor::On);
-	let text = "aaaaaaaaaaaaaaaaaaaaaa";
-	// let width = text.len() as i32 * 6;
-
-	Text::new(text, Point::new(0, 40))
-		.into_styled(text_style)
-		.draw(&mut disp)
-		.expect("Draw text");
-
-	disp.flush().expect("Render display");
-
-	let mut fps = 0u8;
-	timer.start(2_000_000u32); // 1 second
+	led.set_low().unwrap();
 
 	loop {
-		if is_high {
-			led.set_low().unwrap();
-			is_high = false;
-		} else {
-			led.set_high().unwrap();
-			is_high = true;
-		}
-
-		Text::new(text, Point::new(0, 0))
-			.into_styled(text_style)
-			.draw(&mut disp)
-			.unwrap();
-		disp.flush().unwrap();
-
-		fps += 1;
-
-		if timer.read() >= 1_000_000u32 {
-			writeln!(serial, "FPS: {}", fps).unwrap();
-			fps = 0;
-			timer.cancel().unwrap();
-			timer.start(2_000_000u32); // 1 second
-		}
-		// timer.
-
-		// delay(&mut timer, 333_333);
+		// if is_high {
+		// 	led.set_low().unwrap();
+		// 	is_high = false;
+		// } else {
+		// 	led.set_high().unwrap();
+		// 	is_high = true;
+		// }
 	}
 }
 
-fn delay<T>(timer: &mut Timer<T>, cycles: u32)
-where
-	T: Instance,
-{
-	timer.start(cycles);
-	let _ = block!(timer.wait());
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+	free(|cs| {
+		if let Some(ref mut s) = SERIAL.borrow(cs).borrow_mut().deref_mut() {
+			writeln!(s, "{}", info).ok();
+		}
+	});
+
+	loop {}
 }
